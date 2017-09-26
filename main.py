b2b51b64a71e44b0573a0e03b7f679bb918326a2
Aymeric Flaisler <aflaisler@gmail.com>
2017-09-26 12:18:11 +0100

    sw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import numpy as np
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -11,21 +12,22 @@ def index():
     return render_template('index.html')
 
 
+def rand_nb():
+    x = np.random.randint(100, 1000)
+    y = np.random.randint(10, 100)
+    z = sum([int(i) for i in str(x * y)])
+    return x, y, z
+
+
 @app.route('/swim')
 def swim():
-    nbs = []
-    for i in range(5):
-        x = np.random.randint(100, 1000)
-        y = np.random.randint(10, 100)
-        z = sum([int(i) for i in str(x * y)])
-        nbs.append([x, y, z])
-    return render_template('swim.html', nbs=nbs)
+    return render_template('swim.html') + str([rand_nb() for i in range(5)])
 
 
 if __name__ == '__main__':
-    cert = '/etc/letsencrypt/live/aymeric.bio/cert.pem'
-    key = '/etc/letsencrypt/live/aymeric.bio/privkey.pem'
-    context = (cert, key)
-    app.run(host='0.0.0.0', port=443, ssl_context=context,
-            threaded=True, debug=True)
-    # app.run(host='0.0.0.0', debug=True)
+    # cert = '/etc/letsencrypt/live/aymeric.bio/cert.pem'
+    # key = '/etc/letsencrypt/live/aymeric.bio/privkey.pem'
+    # context = (cert, key)
+    # app.run(host='0.0.0.0', port=443, ssl_context=context,
+    #         threaded=True, debug=True)
+    app.run(host='0.0.0.0', debug=True)
