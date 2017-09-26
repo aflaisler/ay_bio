4033336e9d06ff2c795e8ea90fe2d48f06b0d4d7
Aymeric Flaisler <aflaisler@gmail.com>
2017-09-26 11:48:17 +0100

    added swim page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import numpy as np
 
 
 app = Flask(__name__)
@@ -10,9 +11,19 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/swim')
+def swim():
+    x = np.random.randint(100, 1000)
+    y = np.random.randint(10, 100)
+    z = sum([int(i) for i in str(x * y)])
+    nbs = [x, y, z]
+    return render_template('swim.html', nbs=nbs)
+
+
 if __name__ == '__main__':
     cert = '/etc/letsencrypt/live/aymeric.bio/cert.pem'
     key = '/etc/letsencrypt/live/aymeric.bio/privkey.pem'
     context = (cert, key)
-    app.run(host='0.0.0.0',port=443, ssl_context=context, threaded=True, debug=True)
-   # app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=443, ssl_context=context,
+            threaded=True, debug=True)
+    # app.run(host='0.0.0.0', debug=True)
