59b05ebf634de8d1143b07fb750d615459938f81
Aymeric Flaisler <aflaisler@gmail.com>
2018-07-28 16:50:01 +0100

    position_scroll_top

--- static/plugins/back-to-top.js
+++ static/plugins/back-to-top.js
@@ -55,7 +55,7 @@ var scrolltotop={
 			mainobj.cssfixedsupport=!iebrws || iebrws && document.compatMode=="CSS1Compat" && window.XMLHttpRequest //not IE or IE7+ browsers in standards mode
 			mainobj.$body=(window.opera)? (document.compatMode=="CSS1Compat"? $('html') : $('body')) : $('html,body')
 			mainobj.$control=$('<div id="topcontrol">'+mainobj.controlHTML+'</div>')
-				.css({position:mainobj.cssfixedsupport? 'fixed' : 'absolute', bottom:mainobj.controlattrs.offsety+20, right:mainobj.controlattrs.offsetx+28, opacity:0, cursor:'pointer'})
+				.css({position:mainobj.cssfixedsupport? 'fixed' : 'absolute', bottom:mainobj.controlattrs.offsety+28, right:mainobj.controlattrs.offsetx+28, opacity:0, cursor:'pointer'})
 				.attr({title:'Scroll Back to Top'})
 				.click(function(){mainobj.scrollup(); return false})
 				.appendTo('body')
